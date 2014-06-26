8112a237cc9277e56d2ac4d1954ba1ea9a6e19af
Bryan Haskin <bryan@ocgcreative.com>
2014-06-26 11:30:01 -0700

    fixed menu issue

--- lib/modules/shiv/module.php
+++ lib/modules/shiv/module.php
@@ -14,8 +14,10 @@ if( !is_admin() ) :
 	if ( !function_exists( 'ot_load_shives_js' ) ) :
 
 	function ot_load_shives_js() {
-		wp_register_script('respond_js', get_template_directory_uri().'/lib/modules/bootstrap/js/respond.min.js', array("jquery"), '1.3.0', true);
-		wp_register_script('shiv_js', get_template_directory_uri().'/lib/modules/bootstrap/js/html5shiv.js', array(), '1.3.0', false);
+		wp_register_script('respond_js', get_template_directory_uri().'/lib/modules/shiv/js/respond.min.js', array("jquery"), '1.3.0', false);
+		wp_register_script('respondmatchmedia_js', get_template_directory_uri().'/lib/modules/shiv/js/respond.matchmedia.addListener.min.js', array("jquery"), '1.3.0', false);
+		wp_register_script('modernizer_js', get_template_directory_uri().'/lib/modules/shiv/js/modernizer.min.js', array("jquery"), '1.3.0', false);
+		wp_register_script('shiv_js', get_template_directory_uri().'/lib/modules/shiv/js/html5shiv.js', array(), '1.3.0', false);
 	}
 
 	add_action( 'init', 'ot_load_shives_js');
@@ -28,7 +30,10 @@ if( !is_admin() ) :
 		global $is_IE;
 		if($is_IE) {
 			wp_enqueue_script('shiv_js');
+			wp_enqueue_script('respondmatchmedia_js');
 			wp_enqueue_script('respond_js');
+
+			wp_enqueue_script('modernizer_js');
 		}
 	}
 	add_action('wp_print_scripts', 'ot_load_shiv_conditon');
